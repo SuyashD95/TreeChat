2723a9783537a642aa6e05001f4f174ae4d6616a
Suyash <suyashdayal@gmail.com>
2021-02-03 10:52:21 +0530

    Attach OnClickListener to the login w/ Google btn on the welcome screen
    Added the behavior of showing a Toast message for the purposes of testing... 
    
    This behavior will be modified in future when the functionality to let users in using Google's provided authentication will be integrated into the app.

--- Project/app/src/main/java/suyashdayal/android/practice/treechat/MainActivity.java
+++ Project/app/src/main/java/suyashdayal/android/practice/treechat/MainActivity.java
@@ -3,12 +3,32 @@ package suyashdayal.android.practice.treechat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /** Button to let users sign in using their Google's account */
+    TextView mGoogleLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleLoginBtn = findViewById(R.id.google_login_btn);
+        mGoogleLoginBtn.setOnClickListener(this);
+    }
+
+    /**
+     * Called when the button to let users sign in into the application, using
+     * Google's sign in functionality, has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Login with Google", Toast.LENGTH_SHORT).show();
     }
 }
