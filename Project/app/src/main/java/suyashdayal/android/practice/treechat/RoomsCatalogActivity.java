a76a0a6ef81470baeba8ff4abfa87dc43f1c954d
Suyash <suyashdayal@gmail.com>
2021-02-04 07:04:11 +0530

    Attach an listener to send user to the Room linked to specific room item

--- Project/app/src/main/java/suyashdayal/android/practice/treechat/RoomsCatalogActivity.java
+++ Project/app/src/main/java/suyashdayal/android/practice/treechat/RoomsCatalogActivity.java
@@ -2,8 +2,11 @@ package suyashdayal.android.practice.treechat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -15,12 +18,14 @@ import java.util.ArrayList;
 
 public class RoomsCatalogActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = RoomsCatalogActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rooms_catalog);
 
-        ArrayList<String> strings = new ArrayList<>();
+        final ArrayList<String> strings = new ArrayList<>();
         strings.add("Room 1");
         strings.add("Room 2");
         strings.add("Room 3");
@@ -35,6 +40,21 @@ public class RoomsCatalogActivity extends AppCompatActivity {
 
         // Reference to the List of Rooms displayed on the {@link RoomsCatalogActivity}.
         ListView roomsList = findViewById(R.id.rooms_list);
+
+        // Setup a listener to handle each individual click on a specific item
+        roomsList.setOnItemClickListener((parent, view, position, id) -> {
+            String currentRoom = strings.get(position);
+
+            // Create an Intent to move to a specific room represented by {@link RoomActivity}
+            Intent moveToRoom = new Intent(this, RoomActivity.class);
+            // Add the name of the Room to the intent.
+            moveToRoom.putExtra("room_name", currentRoom);
+            // Start {@RoomActivity}
+            startActivity(moveToRoom);
+        });
+
+        // Attach an adapter to retrieve the data source and inflate
+        // the items on the list
         roomsList.setAdapter(stringArrayAdapter);
 
         // The button to create a new Room
