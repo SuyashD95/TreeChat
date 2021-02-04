4b7b8e19b300db266d68a73b904e480c536a0d6e
Suyash <suyashdayal@gmail.com>
2021-02-04 07:08:43 +0530

    Refactor by removing unused import

--- Project/app/src/main/java/suyashdayal/android/practice/treechat/RoomsCatalogActivity.java
+++ Project/app/src/main/java/suyashdayal/android/practice/treechat/RoomsCatalogActivity.java
@@ -3,10 +3,8 @@ package suyashdayal.android.practice.treechat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
