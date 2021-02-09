5c78934e020f8793ba4302d6a634a571d2888ea9
Suyash <suyashdayal@gmail.com>
2021-02-09 15:20:26 +0530

    Add the Model class used to represent the User table in the database

--- API/main.py
+++ API/main.py
@@ -12,8 +12,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # -----------------
 
-# Database Models
-# ---------------
+# Local Database Models
+# ---------------------
+class User(db.Model):
+    """Model class defined for the User table."""
+
+    _id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.relationship('Room', backref='user', lazy=True)
+    sends = db.relationship('Message', backref='user', lazy=True)
+
+    def __repr__(self):
+        """Object representation for a record of User."""
+        return f'User(name={self.name}, email={self.email}, password={self.password})'
 
 
 if __name__ == '__main__':
