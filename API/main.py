a73e702ddfe3c9c14a2864429c4cf2bf4869dea1
Suyash <suyashdayal@gmail.com>
2021-02-09 14:15:49 +0530

    Configure the flask app to use a SQLite database for testing purposes

--- API/main.py
+++ API/main.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
 # App Configuration
 app = Flask(__name__)
 api = Api(app)
+# Local SQLite3 Database Configuration
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 # -----------------
+
+# Database Models
+# ---------------
+
 
 if __name__ == '__main__':
     app.run(debug=True)
